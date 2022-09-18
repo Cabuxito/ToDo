0ff6797d1a8e06b43a613f7fe0cca2b3c9904620
Brian Gastón di Gangi Pedersen <briandigangi@hotmail.com>
2022-09-18 21:40:50 +0200

    +DomainLayer +ServiceLayer +DB

---
+++ ToDo.DB/Tables/ToDoTable.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ToDoTable]
+(
+	[Task_Id] int IDENTITY(1,1) PRIMARY KEY,
+	[TaskName] nvarchar(20),
+	[TaskDescription] nvarchar(25),
+	[CreatedTime] DateTime,
+	[Priority] nvarchar(10),
+	[IsCompleted] bit,
+)
