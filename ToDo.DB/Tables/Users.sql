5ebcf1c76600a6b209f116190b44986a35ec1d90
Brian Gastón di Gangi Pedersen <briandigangi@hotmail.com>
2022-10-03 18:54:42 +0200

    improvements

---
+++ ToDo.DB/Tables/Users.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[User_Id] INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	[Username] nvarchar(50),
+	[Password] nvarchar(50),
+	[FirstName] nvarchar(20),
+	[LastName] nvarchar(20),
+)
---
+++ ToDo.xUnitTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+using ToDo.Domain.Entities;
+
+namespace ToDo.xUnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test_Is_my_accounts_created()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+        } 
+
+
+    }
+}
